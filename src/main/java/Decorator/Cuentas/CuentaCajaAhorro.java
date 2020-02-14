199ecaa0f21a6ae3242983ca09ac4c9d57c425d0
Saul Mamani <luas0_1@yahoo.es>
2020-02-14 16:47:46 -0400

    Patron Facade y Patron decorate

--- src/main/java/Decorator/Cuentas/CuentaCajaAhorro.java
+++ src/main/java/Decorator/Cuentas/CuentaCajaAhorro.java
@@ -6,7 +6,7 @@ public class CuentaCajaAhorro implements ICuenta {
     private double interez = 0.03;
     @Override
     public void abrirCuenta(Cliente cliente) {
-        System.out.println("----- Nueva cuenta Caja de Ahorros");
+        System.out.println("-----| Nueva cuenta Caja de Ahorros");
         System.out.printf("Cliente: %s\nInterez: %f\n", cliente.getNombreCompleto(), this.interez);
     }
 }
--- src/main/java/Decorator/Cuentas/CuentaCorriente.java
+++ src/main/java/Decorator/Cuentas/CuentaCorriente.java
@@ -7,7 +7,7 @@ public class CuentaCorriente implements ICuenta {
     private int plazoFijoAnios = 10;
     @Override
     public void abrirCuenta(Cliente cliente) {
-        System.out.println("----- Nueva cuenta corriente abierta");
+        System.out.println("-----| Nueva cuenta corriente abierta");
         System.out.printf("Cliente: %s\nInterez: %f, Plazo fijo: %d \n", cliente.getNombreCompleto(), this.interez, this.plazoFijoAnios);
     }
 }
