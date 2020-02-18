41f32bdd26044e905e888047bd9ddd6a41dcad5d
saul mamani <luas0_1@yahoo.es>
2020-02-18 01:41:19 -0400

    Patron Interpreter arreglado

--- src/main/java/Interpreter/Main.java
+++ src/main/java/Interpreter/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.printf("c) '%s' es igual a %d\n", expresion, + new Contexto(expresion).evaluar());
 
         expresion = "45 30 + 1 4 - +";
-        System.out.printf("c) '%s' es igual a %d\n", expresion, + new Contexto(expresion).evaluar());
+        System.out.printf("d) '%s' es igual a %d\n", expresion, + new Contexto(expresion).evaluar());
     }
 }
